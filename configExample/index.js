14359d25cce086649c3f7b2a874f3167087e45de
peerzadesaif <saif.pirjade@gmail.com>
2020-09-20 02:31:52 +0530

    Chagned config

--- configExample/index.js
+++ configExample/index.js
@@ -1,24 +1,42 @@
 "use strict";
-export const port = 8001;
-export const host = "127.0.0.1";
-export const environment = "development";
-export const SERVICE_NAME = "admin-platform";
-export const runClusterServer = false;
+module.exports.port = process.env.PORT || 4001;
+module.exports.protocol = 'http'
+module.exports.host = "127.0.0.1";
+module.exports.hostString = "localhost";
+module.exports.clientHostURL = 'http://localhost:3000';
 
-export const mongodb = { uri: "mongodb" };
+module.exports.mongoDBUrl = "null"
 
-export const utils = {
-	PASSWORD_SALT: "SALT",
-	JWT_SECRET: "SALT",
-	JWT_TOKEN_EXPIRE: null,
-	ENCRYPT_SALT: "SALT",
+module.exports.environment = process.env.NODE_ENV || "development";
+module.exports.SERVICE_NAME = "admin-platform";
+module.exports.runClusterServer = false;
+
+module.exports.mongodb = { uri: "mongodb" };
+
+module.exports.options = {
+	reconnectTries: 100,
+	reconnectInterval: 500,
+	autoReconnect: true,
+	//useNewUrlParser: true,
+	dbName: "platform"
+};
+// 
+// SET 
+module.exports.utils = {
+	CRON_WORKER: "*/2 * * * * *",
+	PASSWORD_SALT: "ThisIsMy_Secret_LoL_AdnjssckjFasDkmsRakld",
+	JWT_SECRET: "ThisIsMy_Secret_LoL_GtvhkLRTgtNdGl",
+	JWT_TOKEN_EXPIRE: 2 * 86000, // 2 day
+
+	ENCRYPT_SALT: "ThisIsMy_Secret_LoL_GtvhkLRTgtNdGl",
+	// ENCRYPT_TOKEN_EXPIRE: 2 * 86000, // 2 day
 
 };
 
-export const secret = {
-	NODE_TOKEN_SECRET: "Basic Token",
+module.exports.secret = {
+	NODE_TOKEN_SECRET: "Basic dGVsadszdGluZ0sfdsF1dGg6dGVzdGluZ0BBdXRo",
 };
-export const amazon = {
-	bucketName: "Here",
-	imageUrl: "Route"
+module.exports.amazon = {
+	bucketName: "platform-auxy",
+	imageUrl: "Null"
 };
